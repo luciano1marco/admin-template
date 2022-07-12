d753d878108dbd0d0ff052a1eb3296346b8db2f1
luciano1marco <luciano1marco@gmail.com>
2022-07-12 19:03:12 -0300

    iniciando app

---
+++ src/components/template/MenuLateral.tsx
@@ -0,0 +1,7 @@
+export default function MenuLateral(){
+    return(
+        <aside>
+            <h1>MenuLateral</h1>
+        </aside>
+    )
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import Layout from "../components/template/Layout";
+
+export default function Home() {
+  return (
+   
+     <Layout titulo="Pagina inicial" subtitulo="Estamo contrundo um layout">
+        <h3>Conteudo....</h3>
+     </Layout>
+   
+  )
+}
