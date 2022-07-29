9d7da3db47717406e4364ef05cf5f65cfa7d60ab
Luciano Correa Marco <luciano.correa@riogrande.local>
2022-07-29 11:10:43 -0300

    tela de autenticação

--- src/pages/autenticacao.tsx
+++ src/pages/autenticacao.tsx
@@ -7,31 +7,83 @@ export default function Autenticacao(){
     const[email, setEmail] = useState('')
     const[senha, setSenha] = useState('')
     
+    function submeter(){
+        if(modo === 'login'){
+            console.log('login')
+        } else{
+            console.log('cadastrar')
+        }
+    }
+
     return(
-        <div >
-           <h1>Autenticação</h1>
-            <AuthInput
-                label="Email"
-                tipo="email"
-                valor={email}
-                valorMudou={setEmail}
-                obrigatorio
-            />
-            <AuthInput
-                label="Senha"
-                tipo="password"
-                valor={senha}
-                valorMudou={setSenha}
-                obrigatorio
-            />
-            {/*<AuthInput
-                label="Confirmar Senha"
-                tipo="password"
-                valor={senha}
-                valorMudou={setSenha}
-                obrigatorio
-                naoRenderizarQuando= {false}
-            /> */}
+        <div className={`flex h-screen items-center justify-center`}>
+            <div className={`hidden md:block md:w-1/2 lg:w-2/3`}>
+                <img    src="https://source.unsplash.com/random" 
+                        alt="Imagem da tela de autenticação" 
+                        className="h-screen w-full object-cover " />
+            </div>
+            <div className={`m-10 w-full md:w-1/2 w-1/3 `}>
+                <h1 className={`text-3xl font-bold mb-5
+                                `}>
+                        {modo === 'login' ? 'Entre com sua conta' : 'Cadastre-se na plataforma'}
+
+                </h1>
+                    <AuthInput
+                        label="Email"
+                        tipo="email"
+                        valor={email}
+                        valorMudou={setEmail}
+                        obrigatorio
+                    />
+                    <AuthInput
+                        label="Senha"
+                        tipo="password"
+                        valor={senha}
+                        valorMudou={setSenha}
+                        obrigatorio
+                    />
+                    {/*<AuthInput
+                        label="Confirmar Senha"
+                        tipo="password"
+                        valor={senha}
+                        valorMudou={setSenha}
+                        obrigatorio
+                        naoRenderizarQuando= {false}
+                    /> */}
+                    <button onClick={submeter} 
+                            className={`  w-full bg-indigo-500 hover:bg-indigo-400
+                                        text-white rounded-lg px-4 py-3 mt-6  
+                                    `}>
+                        {modo === 'login' ? 'Entrar' : 'Cadastrar' }
+                    </button>
+
+                    <hr className={` my-6 border-gray-300 w-full`}/>
+
+                    <button onClick={submeter} 
+                            className={`  w-full bg-red-500 hover:bg-red-400
+                                        text-white rounded-lg px-4 py-3 
+                                    `}>
+                        Entrar com Google
+                    </button>
+                    {modo === 'login' ? (
+                        <p className="mt-8"> 
+                            Novo por Aqui?
+                            <a onClick={() => setModo('cadastro')}
+                               className={` text-blue-500 hover:text-blue-700 font-semibold cursor-pointer`}
+                            > Crie uma Conta gratis</a>
+                            
+                         </p>   
+                    ):(
+                        <p className="mt-8"> 
+                            Já faz parte da comunidade?
+                            <a onClick={() => setModo('login')}
+                               className={` text-blue-500 hover:text-blue-700 font-semibold cursor-pointer`}
+                            > Entre com suas credenciais</a>
+                         </p> 
+
+                    )
+                }
+            </div>
         </div>
     )
 }
